1d5b61db79a53f1d58855704f9ed74148920a10d
Jstewart3313 <Justinstewart3313@gmail.com>
2020-06-09 13:05:57 -0400

    third media fix

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -21,7 +21,7 @@ export default function Intro() {
                 Serving in the United States Army for 5 years brought so much perspective into my life at such a young age. It showed me that hard work and self discpline can open doors that I couldn't even dream of. This has enstilled in me a work ethic that is driven by passion. Understanding your work is only part of success, but LOVING your work will tranform it from good to great.
               </div>
               <div className='intro-p3'>
-                Highly self-motivated but infatuated by being inspired by others. I love articulating technical terms and collaborating with others. I'm a fast learner and am able to switch roles and and project context with relative ease. I believe in expertise and appreciate honing in on my skills until I become highly proficient.
+                Highly self-motivated but infatuated by being inspired by others. I love articulating technical terms and collaborating with others. I'm a fast learner and am able to switch roles and project context with relative ease. I believe in expertise and appreciate honing in on my skills until I become highly proficient.
               </div>
               <div className='intro-p4'>
                 At my core I am a empathetic, compassionate human. I find fullfillment in creatively expressing myself with others. Understanding whats drives people and how we can all truly learn something from everyone we encounter is something I will never let go. 
