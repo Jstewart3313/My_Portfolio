4e13fc418430e4f3e3e198b494b4a49f8b7cd780
Jstewart3313 <Justinstewart3313@gmail.com>
2020-08-26 17:48:04 -0400

    Add contact info and socials

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import './App.css';
 import Header from "./Components/Header/Header.js"
 import Photo from "./Components/Photo/Photo.js"
 import Intro from "./Components/Intro/Intro.js"
+import Contact from "./Components/Contact/Contact.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className='body'>
-       <Header/>
-       <Photo/>
-       <Intro/>
-       </div>
+          <Header />
+          <Photo />
+          <Intro />
+          <Contact />
+        </div>
       </div>
     );
   }
